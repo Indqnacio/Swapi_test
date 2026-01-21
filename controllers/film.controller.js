99d833bae50c136f2a78d31bd179df257783d8ee
indQnac10 <indqnacio@gmail.com>
2026-01-21 16:01:22 -0700

    probando en caso de que funcione el endpoint de getFilmPage

--- controllers/film.controller.js
+++ controllers/film.controller.js
@@ -1,3 +1,7 @@
+//*parametros en req.query
+//*Id
+//*page
+//*body
 const Film = require("../models/film.model");
 
 exports.getFilmById = async (req, res) => {
@@ -5,13 +9,27 @@ exports.getFilmById = async (req, res) => {
     const film = await Film.findById(req.params.id);
     res.json(film);
   } catch (err) {
-    res.status(404).json({ error: 'Film no encontrado' });
+    res.status(404).json({ error: "Film no encontrado" });
   }
 };
 
 //no se todavia como pero tengo que hacer que obtenga 10 chars
 //y que lo haga de modo ordenado
 //talvez lo ideal sea ordenado alphabeticamente
+exports.getFilmPage = async (req, res) => {
+  const page = Number(req.query.page) || 1;
+  const limit = 10;
+  try {
+    const films = await Film.find()
+      .skip((page + 1) * limit)
+      .limit(limit);
+      
+    res.json(films);
+  } catch (error) {
+    res.status(500).json({ error: "Ha ocurrido un error al insertar el film" });
+  }
+};
+
 exports.postFilm = async (req, res) => {
   try {
     const newFilm = await Film.create(req.body);
@@ -23,12 +41,10 @@ exports.postFilm = async (req, res) => {
 
 exports.editFilm = async (req, res) => {
   try {
-    const film = await Film.findByIdAndUpdate(
-        req.params.id, 
-        req.body,
-         { new: true }
-        );
-        res.json(film);
+    const film = await Film.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(film);
   } catch (error) {
     res.status(500).json({ error: "Ha ocurrido un error al editar el film" });
   }
@@ -42,5 +58,3 @@ exports.deleteFilm = async (req, res) => {
     res.status(500).json({ error: "Ha ocurrido un error al borrar el film" });
   }
 };
-
-
